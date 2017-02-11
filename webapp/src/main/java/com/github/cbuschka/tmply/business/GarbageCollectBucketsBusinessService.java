c823c82e065da899cdf7b06906344e7e43a4c32a
conni <c.buschka@arcusx.com>
2017-02-11 14:25:47 +0100

    Added logging of bucket eviction.

--- webapp/src/main/java/com/github/cbuschka/tmply/business/GarbageCollectBucketsBusinessService.java
+++ webapp/src/main/java/com/github/cbuschka/tmply/business/GarbageCollectBucketsBusinessService.java
@@ -1,6 +1,8 @@
 package com.github.cbuschka.tmply.business;
 
 import com.github.cbuschka.tmply.domain.BucketRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
@@ -8,11 +10,18 @@ import org.springframework.stereotype.Service;
 @Service
 public class GarbageCollectBucketsBusinessService
 {
+	private static Logger log = LoggerFactory.getLogger(GarbageCollectBucketsBusinessService.class);
+
 	@Autowired
 	private BucketRepository bucketRepository;
 
-	@Scheduled(initialDelay = 1000*30, fixedDelay = 1000*5)
+	@Scheduled(initialDelay = 1000*30, fixedDelay = 1000*30)
 	public void evictBuckets() {
+
+		log.info("Started bucket eviction.");
+
 		this.bucketRepository.evictBuckets();
+
+		log.info("Bucket eviction done.");
 	}
 }
